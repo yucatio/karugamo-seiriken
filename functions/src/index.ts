c36200f0b3a11e3b789f76f98751183b9d8f4844
yucatio <yucatio.com@gmail.com>
2022-01-26 11:27:46 +0900

    Fixed by lint warning

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,36 @@
-import * as functions from "firebase-functions"
-import * as admin from "firebase-admin"
-admin.initializeApp()
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
 // 整理券の保持期間
 // 変更する場合はBusinessSettings > ticketExpiryDays も更新する
-const ticketExpiryDays = 30
+const ticketExpiryDays = 30;
 
 // 古い整理券を削除する
-// 定期的なジョブにすべきだが、料金がかかるので一旦Restにしておく
-export const deleteOldTicketEvents = functions.https.onRequest(async (request, response) => {
-  const db = admin.firestore()
-  
-  const threshold = new Date()
-  // set to start of the day
-  threshold.setHours(0, 0, 0, 0)
-  threshold.setDate(threshold.getDate() - ticketExpiryDays)
+export const deleteOldTicketEvents =
+  functions.https.onRequest(async (request, response) => {
+    const db = admin.firestore();
 
-  const query = db.collection("ticket-events").where("createdAt", "<", threshold)
-  const querySnapshot = await query.get()
-  
-  querySnapshot.forEach((doc) => {
-    const docData = doc.data()
-    functions.logger.info("delete ticket-event",{
-      id: doc.id,
-      title: docData.title,
-      owner: docData.owner,
-      createdAt: docData.createdAt.toDate().toISOString(),
-      updatedAt: docData.updatedAt.toDate().toISOString(),
-    })
-    const docRef = db.doc(doc.ref.path)
-    db.recursiveDelete(docRef)
-  })
+    const threshold = new Date();
+    // set to start of the day
+    threshold.setHours(0, 0, 0, 0);
+    threshold.setDate(threshold.getDate() - ticketExpiryDays);
 
-  response.send(`Delete count: ${querySnapshot.size}`)
-})
+    const query = db.collection("ticket-events")
+        .where("createdAt", "<", threshold);
+    const querySnapshot = await query.get();
+
+    querySnapshot.forEach((doc) => {
+      const docData = doc.data();
+      functions.logger.info("delete ticket-event", {
+        id: doc.id,
+        title: docData.title,
+        owner: docData.owner,
+        createdAt: docData.createdAt.toDate().toISOString(),
+        updatedAt: docData.updatedAt.toDate().toISOString(),
+      });
+      db.recursiveDelete(db.doc(doc.ref.path));
+    });
+
+    response.send(`Delete count: ${querySnapshot.size}`);
+  });
